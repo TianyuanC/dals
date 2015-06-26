b35411a3b9d6c5a22a515a479d5abbe47bc04d93
Ryan Chu <tianyuan.chu@gmail.com>
2015-06-26 15:35:52 -0700

    integration tests

---
+++ ClientRepository.IntegrationTests/NugetTest.cs
@@ -0,0 +1,57 @@
+﻿namespace ClientRepository.IntegrationTests
+{
+    using DALs.Http;
+    using DALs.Model;
+    using DALs.Model.Interfaces;
+    using DALs.Sql;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class NugetTest.
+    /// </summary>
+    public class NugetTest
+    {
+        #region Members
+        /// <summary>
+        /// The sprocs
+        /// </summary>
+        private readonly ISprocs sprocs;
+        /// <summary>
+        /// The web client
+        /// </summary>
+        private readonly IWebClient webClient;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NugetTest"/> class.
+        /// </summary>
+        public NugetTest():this(new Sprocs(), new WebClient())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NugetTest"/> class.
+        /// </summary>
+        /// <param name="sprocs">The sprocs.</param>
+        /// <param name="webClient">The web client.</param>
+        public NugetTest(ISprocs sprocs, IWebClient webClient)
+        {
+            this.sprocs = sprocs;
+            this.webClient = webClient;
+        }
+        #endregion
+
+        /// <summary>
+        /// Testses this instance.
+        /// </summary>
+        /// <returns>Task.</returns>
+        public async Task Tests()
+        {
+            var config = new SqlSprocConfiguration();
+            await sprocs.ExecuteAsync(config, (dataReader) => 1);
+            var httpConfig = new HttpClientConfig();
+            await webClient.GetAsync(httpConfig, (response) => 1);
+        }
+    }
+}
