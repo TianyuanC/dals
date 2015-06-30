3e5350e74686d023d73725193596b5e4bcef0645
tianyuan.chu <tianyuan.chu@gmail.com>
2015-06-29 21:48:06 -0700

    mock httpclient with message handler

--- DALs.UnitTests/DALsHttp/RestClientTests.cs
+++ DALs.UnitTests/DALsHttp/RestClientTests.cs
@@ -3,6 +3,16 @@
     using DALs.Http;
     using NUnit.Framework;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using ClientRepository.Model;
+    using DALs.Model.Configs;
+    using DALs.Model.Enums;
+    using DALs.Model.Interfaces;
+    using NSubstitute;
 
     [TestFixture]
     public class RestClientTests
@@ -20,21 +30,74 @@
             Assert.AreEqual("init", ex.ParamName);
         }
 
-        //[Test]
-        //public void GetAsync()
-        //{
-        //    var init = Substitute.For<IInitProvider>();
-        //    init.HttpClient.Returns(new HttpClient(new FakeMessageHandler()));
-        //    var rest = new RestClient(init);
-        //    //ready to mock
-        //}
+        [Test]
+        public async Task GetAsync()
+        {
+            var init = Substitute.For<IHttpInitializer>();
+            var messager = new FakeMessageHandler(new HttpResponseMessage());
+            init.HttpClient.Returns(new HttpClient(messager));
+            var rest = new RestClient(init);
+            var config = new HttpConfiguration(new Uri("http://happy.ca/"), "route", HttpRequest.Get);
+            
+            var ads = await rest.GetAsync(config, response => new List<Ad>{new Ad{Id = long.MaxValue}});
 
-        //private class FakeMessageHandler : HttpMessageHandler
-        //{
-        //    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
+            Assert.AreEqual(1, ads.Count(x=>x.Id==long.MaxValue));
+            Assert.AreEqual("http://happy.ca/route", messager.RequestMessage.RequestUri.AbsoluteUri);
+            Assert.AreEqual(HttpMethod.Get, messager.RequestMessage.Method);
+        }
+
+        [Test]
+        public async Task SetSyncPut()
+        {
+            var init = Substitute.For<IHttpInitializer>();
+            var messager = new FakeMessageHandler(new HttpResponseMessage());
+            init.HttpClient.Returns(new HttpClient(messager));
+            var rest = new RestClient(init);
+            var config = new HttpConfiguration(
+                new Uri("http://happy.ca/"), "route", HttpRequest.Put) {Data = "data"};
+
+            var ads = await rest.SetAsync(config, response => 
+                new List<Ad> { new Ad { Id = long.MaxValue } });
+
+            Assert.AreEqual(1, ads.Count(x => x.Id == long.MaxValue));
+            Assert.AreEqual("http://happy.ca/route", messager.RequestMessage.RequestUri.AbsoluteUri);
+            Assert.AreEqual(HttpMethod.Put, messager.RequestMessage.Method);
+        }
+
+        [Test]
+        public async Task SetSyncPost()
+        {
+            var init = Substitute.For<IHttpInitializer>();
+            var messager = new FakeMessageHandler(new HttpResponseMessage());
+            init.HttpClient.Returns(new HttpClient(messager));
+            var rest = new RestClient(init);
+            var config = new HttpConfiguration(
+                new Uri("http://happy.ca/"), "route", HttpRequest.Post) { Data = "data" };
+
+            var ads = await rest.SetAsync(config, response =>
+                new List<Ad> { new Ad { Id = long.MaxValue } });
+
+            Assert.AreEqual(1, ads.Count(x => x.Id == long.MaxValue));
+            Assert.AreEqual("http://happy.ca/route", messager.RequestMessage.RequestUri.AbsoluteUri);
+            Assert.AreEqual(HttpMethod.Post, messager.RequestMessage.Method);
+        }
+
+        private class FakeMessageHandler : HttpMessageHandler
+        {
+            private readonly HttpResponseMessage response;
+
+            public FakeMessageHandler(HttpResponseMessage response)
+            {
+                this.response = response;
+            }
+
+            public HttpRequestMessage RequestMessage { get; private set; }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                RequestMessage = request;
+                return Task.FromResult(response);
+            }
+        }
     }
 }
